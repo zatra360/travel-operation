4a277a31ecbf0d13a6284eaf37c9dfef12e9dd42
Mahidul Islam Nakib <mahidulislamnakib@gmail.com>
2026-07-08 22:29:59 +0600

    fix: add ActivityModule import to LeadModule and ClientModule

--- apps/api/src/modules/client/client.module.ts
+++ apps/api/src/modules/client/client.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ClientController } from './client.controller';
 import { ClientService } from './client.service';
+import { ActivityModule } from '../activity/activity.module';
 
 @Module({
+  imports: [ActivityModule],
   controllers: [ClientController],
   providers: [ClientService],
   exports: [ClientService],
--- apps/api/src/modules/lead/lead.module.ts
+++ apps/api/src/modules/lead/lead.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LeadController } from './lead.controller';
 import { LeadService } from './lead.service';
+import { ActivityModule } from '../activity/activity.module';
 
 @Module({
+  imports: [ActivityModule],
   controllers: [LeadController],
   providers: [LeadService],
   exports: [LeadService],
